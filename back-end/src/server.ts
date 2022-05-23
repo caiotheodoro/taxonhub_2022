fdf5d814769342a5a09c58d94905b5357e4d22e6
gustavo asato <gustavokioshiasato@gmail.com>
2022-05-23 05:12:10 -0300

    create swagger documentation

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
-
+import swaggerUi from "swagger-ui-express";
 import managerCron from './manager-cron';
+import swaggerDocument from './swagger.json'
 import { EMetaTableValues } from './modules/wfo/enumerators/types';
 import { wfoRepository } from './modules/wfo/repositories';
 import { router } from './routes';
@@ -11,12 +12,14 @@ const port = process.env.PORT || 3333;
 app.use(express.json());
 app.use(router);
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.get('/', (_, res) => res.send('API UP'));
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 
-    wfoRepository.updateDatabasePhaseStatus(EMetaTableValues.needToCheck);
+    //wfoRepository.updateDatabasePhaseStatus(EMetaTableValues.needToCheck);
 
     managerCron.run();
 });
